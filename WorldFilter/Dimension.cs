ecfa835fdc8e6c30a7d09e53088a4dfa90da8c27
Dw4lt <142900824+Dw4lt@users.noreply.github.com>
2023-08-28 15:03:07 +0200

    Add World & Dimension classes

---
+++ WorldFilter/Dimension.cs
@@ -0,0 +1,27 @@
+﻿namespace WorldFilter {
+    public class Dimension {
+        DirectoryInfo DimensionDir;
+
+        public Dimension(DirectoryInfo dimensionDir) {
+            DimensionDir = dimensionDir;
+        }
+
+        public IEnumerable<RegionFile> GetRegionFiles() {
+            var region_dir = DimensionDir.EnumerateDirectories().FirstOrDefault(d => d.Name == "region");
+            if (region_dir != null) {
+                foreach (var file in region_dir.GetFiles("r.*.mca", new EnumerationOptions())) {
+                    RegionFile? region = null;
+                    try {
+                        region = RegionFile.Open(file);
+                    } catch (Exception e) {
+                        Console.WriteLine(e.ToString());
+                        Console.Write(e.StackTrace);
+                    }
+                    if (region != null) {
+                        yield return region;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ WorldFilter/World.cs
@@ -0,0 +1,47 @@
+﻿
+using fNbt;
+using System.ComponentModel.Design;
+using System.Diagnostics.CodeAnalysis;
+
+namespace WorldFilter {
+    public class World {
+        NbtCompound RootTag;
+        DirectoryInfo WorldDir;
+
+        public World(DirectoryInfo root_dir) {
+            if (root_dir is null) throw new ArgumentNullException("Could not fetch root dir of world");
+            var files = root_dir.GetFiles("level.dat");
+            foreach (FileInfo f in files) {
+                RootTag = ReadLevelFile(f);
+                break;
+            }
+            if (RootTag is null) throw new Exception("World does not contain level file.");
+            WorldDir = root_dir;
+        }
+
+        private NbtCompound ReadLevelFile(FileInfo path) {
+            return new NbtFile(path.FullName).RootTag;
+        }
+
+        public IEnumerable<Dimension> GetDimensions() {
+            yield return new Dimension(WorldDir);
+            foreach (var subdir in WorldDir.GetDirectories()) {
+                switch (subdir.Name) {
+                    case "DIM1":
+                    case "DIM-1":
+                        yield return new Dimension(subdir);
+                        break;
+                    case "dimensions":
+                        foreach(var group in subdir.GetDirectories()) {
+                            foreach (var custom_dim in group.GetDirectories()) {
+                                yield return new Dimension(custom_dim);
+                            }
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
